d8bbd7e492cfea048993335035a3a8a09f5e621e
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-09-15 18:57:08 +0200

    Add files via upload

---
+++ Ponavljanje-zad9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_zad9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 15, 26, 17, 194, 14, 70 };
+
+            Array.Sort(arr);
+
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
